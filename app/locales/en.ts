a9f70cf40764896d205010648e2cbcb159f66231
WenqiOfficial <2747297361@qq.com>
2025-02-28 21:48:13 +0800

    update

--- app/locales/en.ts
+++ app/locales/en.ts
@@ -26,7 +26,7 @@ const en: LocaleType = {
     Later: "Later",
     SaasTips: "Too Complex, Use Immediately Now",
     TopTips:
-      "🥳 NextChat AI launch promotion: Instantly unlock the latest models like OpenAI o1, GPT-4o, Claude-3.5!",
+      "If you don't have an",
   },
   ChatItem: {
     ChatItemCount: (count: number) => `${count} messages`,
